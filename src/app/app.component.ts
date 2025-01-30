7533a52e6afa501e210f2961b6282059b827e8b2
saif <saif.blt17@gmail.com>
2025-01-30 22:50:10 +0530

    configurable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,23 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
-  onSelectUser(id: String) {
-    console.log(id)
+  selectedUserID = 'u1';
+
+  get selectedUser() {
+      return this.users.find((user) => user.id === this.selectedUserID)!
+  }
+  onSelectUser(id: string) {
+    this.selectedUserID = id;
   }
 }
